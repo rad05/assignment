38fbe011437e479d73c9b342c6402d7f7397b37f
rad05 <rad7804@gmail.com>
2014-11-26 18:20:31 +0530

    Changed filename from index1.php to index

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div id="header">
+		My library
+	</div>
+	<div id="footer">
+		&copy My library
+	</div>
+</body>
+</html>
+
+<?php
+include "showlist.php";
+showlist();
+?>
+
+
+
